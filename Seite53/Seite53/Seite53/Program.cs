27231b7762e21262790e6f06f199b1141d1a78f0
DSchndr <dennix1010@gmail.com>
2019-12-04 20:21:39 +0100

    Seite 53 + anderes zeugs

---
+++ Seite53/Seite53/Seite53/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace Seite53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                A1();
+                A2();
+                A3();
+                A4();
+                A5_1();
+                A5_2();
+            }
+            catch
+            {
+                Console.WriteLine("Ürgendwas sehr sehr doofes ist passiert :/");
+            }
+            Console.WriteLine("Done. Press zhe ender Taste.");
+            Console.ReadLine();
+        }
+
+        static bool A1()
+        {
+            Console.Write("Problemstellung 1: Anzahl der Vorfahren\n Anzahl der Generationen: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n == 0) return false;
+            Console.WriteLine("{0} Vorfahren vor {1} Generationen.", Math.Pow(2, n), n); //2 hoch die Anzahl der generationen
+            return true;
+        }
+
+        // Der code sieht richtig scheisse aus und nichtmal ich versteh den, AAAABER er funktioniert! :D
+        static void A2()
+        {
+            Console.WriteLine("\nProblemstellung 2: Kleines 1 mal 1");
+            string b = ""; //temporary variable for first column
+            bool tmp = false; //temporary variable used to reset x back to -1
+            for (int x = -1; x <= 10; x++)
+            {
+                if (x == 0) //2te zeile hat nur "-"
+                {
+                    for (int a = 0; a < 100; a++)
+                    {
+                        Console.SetCursorPosition(a, Console.CursorTop);
+                        Console.Write("-");
+                    }
+                    Console.Write("\n");
+                    continue; // raus mit de ~viechers~ for loop
+                }
+                if (x == -1) //1ste zeile hat den stern im ersten feld
+                {
+                    b = "*";
+                    x = 1;
+                    tmp = true;
+                }
+                else if (x > 0)
+                { //3 bis nte zeile hat eine zahl im ersten feld
+                    b = x.ToString();
+                }
+                Console.WriteLine("\t{0}\t|\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{9}\t{10}", b, 1 * x, 2 * x, 3 * x, 4 * x, 5 * x, 6 * x, 7 * x, 8 * x, 9 * x, 10 * x); //zeile ausgeben
+                if (tmp) { x = -1; tmp = false; } //x wieder auf -1 zurücksetzen
+            }
+        }
+        static void A3()
+        {
+            Console.WriteLine("\nProblemstellung 3: Formen");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("\n");
+                for (int a = 1; a <= i; a++)
+                {
+                    Console.SetCursorPosition(a, Console.CursorTop);
+                    Console.Write("#");
+                }
+            }
+            Console.Write("\n");
+            for (int i = 10; i >= 1; i--) //das ganze im rückwärtsgang
+            {
+                Console.Write("\n");
+                for (int a = 1; a <= i; a++)
+                {
+                    Console.SetCursorPosition(a, Console.CursorTop);
+                    Console.Write("#");
+                }
+            }
+            Console.Write("\n");
+            for (int i = 10; i >= 1; i--) // das ganze im gang P
+            {
+                Console.Write("\n");
+                for (int a = 0; a <= ((10 - i) * 2); a++)
+                {
+                    Console.SetCursorPosition(i + a, Console.CursorTop);
+                    Console.Write("#");
+                }
+            }
+        }
+        static void A4()
+        {
+            Console.WriteLine("\nProblemstellung 4: Eingabenkontrolle");
+            while (A1()) {; } // A1 gibt false zurück wenn 0 eingeben wurde -> loop macht kein loop mehr :(
+        }
+        static void A5_1()
+        {
+            Console.WriteLine("\nProblemstellung 5: 2 Bedingungen (L1)");
+            bool Bool = true;
+            int tmp = ' ';
+            while (Bool)
+            {
+                Console.WriteLine("*");
+                tmp = Console.Read();
+                if (tmp == 'J' || tmp == 'j') Bool = false;
+            }
+        }
+        static void A5_2()
+        {
+            Console.WriteLine("\nProblemstellung 5: 2 Bedingungen (L2)");
+        
+        // Is this basic or c#?
+        A:
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.J) return;
+            }
+            Console.WriteLine("*");
+            goto A;
+        }
+    }
+}
+
---
+++ test2/test2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+class Test2
+{
+    /* 
+    
+    *** Playground for random crap. ***
+    
+    */
+    public static void Main()
+    {
+        //string path = @"/home/dennis/test1.sh";
+        //decodeandlaunch("IyEvYmluL2Jhc2gKZWNobyAiSGFsbG8hIgpyZWFkCg==", path);
+        Test2 test2 = new Test2();
+        //test2.nyanneee();
+        //test2.algo();
+        Console.ReadLine();
+        test2.test();
+        Console.ReadLine();
+
+    }
+
+    public void test() {
+        Console.BackgroundColor = ConsoleColor.DarkRed;
+        for(int x = 0; x <= Console.WindowWidth; x++) {
+            for(int y = 0; y <= Console.WindowHeight; y++) {
+                Console.SetCursorPosition(x,y);
+                Console.Write(" ");
+            }
+        }
+    }
+
+    private String[] nyanneee_str = new string[] {
+        @"Nyan catu-chan",
+        @"",
+        @"___━*━___━━___━__*___━_*___┓━╭­­­­­━━━━╮",
+        @"__*_━━___━━___━━*____━━___┗┓|:­­­­­:::::^---^",
+        @"___━━___━*━___━━____━━*___━┗|:­­­­­:::|｡◕‿‿­­­­◕｡|",
+        @"___*━__━━_*___━━___*━━___*━━╰O­­­­­--O---O--O ╯"
+
+    };
+    public void nyanneee()
+    {
+        foreach (string s in nyanneee_str)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+
+
+    // decodeandlaunch("QGVjaG8gb2ZmDQplY2hvIGhhbGxvIDpE"); //schreibt zu c:\test.bat und führt aus
+    // decodeandlaunch("QGVjaG8gb2ZmDQplY2hvIGhhbGxvIDpE", @"c:\hallo.bat"); //schreibt zu c:\hallo.bat und führt aus
+    public static void decodeandlaunch(string b64string, string path = @"c:\test.bat")
+    {
+        File.WriteAllBytes(path, Convert.FromBase64String(b64string));
+
+        Process process;
+        ProcessStartInfo processInfo;
+        //processInfo = new ProcessStartInfo("cmd.exe", "/c " + path);  //Windows
+        processInfo = new ProcessStartInfo("xterm", "-hold -e " + path);          //Linux
+        processInfo.CreateNoWindow = false;
+        processInfo.UseShellExecute = false;
+
+        process = Process.Start(processInfo);
+        process.WaitForExit();
+        File.Delete(path);
+    }
+}
