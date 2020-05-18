1199559eacbcfa89ad04b9434ef47be420f5d53b
DSchndr <dennix1010@gmail.com>
2020-05-19 00:57:38 +0200

    [Seite76] Added Schiffeversenken

--- Seite76/a3/a3/Program.cs
+++ Seite76/a3/a3/Program.cs
@@ -1,13 +1,89 @@
 ﻿using System;
 
-namespace a3
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //TODO: Write code
-            
+namespace a3 {
+    class Program {
+        static void Main (string[] args) {
+            //Spielfeld array erstellen & mit . füllen
+            char[, ] Spielfeld = new char[10, 10];
+            for (int x = 0; x < 10; x++) {
+                for (int y = 0; y < 10; y++) {
+                    Spielfeld[x, y] = '.';
+                }
+            }
+            char[, ] Schiffe = new char[10, 10];
+
+            setzenSchiffe (ref Schiffe);
+            ausgebenSpielfeld (ref Spielfeld, true);
+
+            //gameloop
+            bool gameloop = true;
+            while (gameloop) {
+                ausgebenSpielfeld (ref Spielfeld);
+                schiessen (ref Spielfeld, ref Schiffe);
+                gameloop = ende (ref Schiffe);
+            }
+            Console.WriteLine("Spiel gewonnen! Blah blah blah...\n Programmierer ist hier pennen gegangen und hatte kein Bock mehr.");
+
+        }
+
+        static void ausgebenSpielfeld (ref char[, ] arr, bool redraw = false) {
+            if (redraw) {
+                //zahlen am rand setzen
+                for (int x = 0; x < 10; x++) {
+                    Console.SetCursorPosition (x, 0);
+                    if (x == 0) {; } else if (x == 10) Console.Write ("0");
+                    else Console.Write (x);
+                }
+                for (int y = 0; y < 10; y++) {
+                    Console.SetCursorPosition (0, y);
+                    if (y == 0) Console.Write (" ");
+                    else if (y == 10) Console.Write ("0");
+                    else Console.Write (y);
+                }
+            }
+            //punkte im spielfeld setzen
+            for (int x = 1; x < 10; x++) {
+                for (int y = 1; y < 10; y++) {
+                    Console.SetCursorPosition (x, y);
+                    Console.Write (arr[x, y]);
+                }
+            }
+
+        }
+        static void setzenSchiffe (ref char[, ] arr) {
+            arr[1, 2] = 'S';
+            arr[5, 6] = 'S';
+            arr[4, 3] = 'S';
+        }
+        static void schiessen (ref char[, ] SParr, ref char[, ] Sarr) {
+            A : 
+            Console.SetCursorPosition (0, 12);
+            Console.Write ("X-Koordinate: ");
+            String xs = Console.ReadLine ();
+            Console.Write ("\nY-Koordinate: ");
+            String ys = Console.ReadLine ();
+            if (!Int32.TryParse (xs, out int x)) {
+                goto A;
+            }
+            if (!Int32.TryParse (ys, out int y)) {
+                goto A;
+            }
+            if (Sarr[x, y] == 'S') {
+                Sarr[x, y] = 'X';
+                SParr[x, y] = 'X';
+            } else {
+                SParr[x, y] = '*';
+            }
+
+        }
+
+        //Hat das array ein Schiff? -> loop weiter laufenlassen
+        static bool ende (ref char[, ] arr) {
+            bool ret = false;
+            foreach (char c in arr) {
+                if (c == 'S') ret = true;
+            }
+            return ret;
         }
     }
 }
---
+++ tst3/tst3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tst3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	
+        	int b;
+            do {
+            	Console.WriteLine("Betrag eingeben: ");
+            	b = Convert.ToInt16(Console.ReadLine());
+            } while ((b<=0) || (b>=100));
+            int f = b/5;
+            int e = (b%5)/2;
+            int z = ((b%5)%2);
+            Console.WriteLine("f: {0}");
+        }
+    }
+}
